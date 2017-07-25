5eeb6b7df1ad83bd4e6d524112a5aef342fdaf8f
Adamus <adamus_7@outlook.com>
2017-07-26 00:18:37 +0800

    Add like

--- pages/bus/bus.js
+++ pages/bus/bus.js
@@ -11,7 +11,7 @@ Page({
     console.log(option.type)
     app.getShuttlBusList(function (ShuttleBusList) {
       //更新数据
-      switch(option.type){
+      switch (option.type) {
         case 'r':
           that.setData({
             targetBus: ShuttleBusList.RegularRouteShuttle[option.id]
@@ -29,6 +29,38 @@ Page({
           break
       }
     })
-
+  },
+  onLikeTap: function (e) {
+    var that = this
+    console.log("onLikeTab")
+    // wx.setStorage({
+    //   key: "likedRegularRoute",
+    //   data: "value"
+    // })
+    // wx.setStorage({
+    //   key: 'likedRegularRoute',
+    //   data: [that.data.targetBus]
+    // })
+    wx.getStorage({
+      key: 'likedRegularRoute',
+      success: function (res) {
+        var data = res.data
+        console.log(data)
+        console.log(that.data.targetBus)
+        if (!data.includes(that.data.targetBus)) {
+          //data.push(that.data.targetBus)
+          // wx.setStorage({
+          //   key: 'likedRegularRoute',
+          //   data: data
+          // })
+        }
+      },
+      fail: function () {
+        wx.setStorage({
+          key: 'likedRegularRoute',
+          data: [that.data.targetBus]
+        })
+      }
+    })
   }
 })
