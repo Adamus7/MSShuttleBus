3823f6e4932a1a157e7ca8dca5b3e42278b38717
Adamus <adamus_7@outlook.com>
2018-08-10 16:37:53 +0800

    Fix search bar
    Bring search bar back on list page.

--- pages/list/list.js
+++ pages/list/list.js
@@ -3,18 +3,22 @@
 var app = getApp()
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // Init data
   data: {
-    ShuttleBusList:[]
+    ShuttleBusList: [],
+    //Search Bar
+    inputShowed: false,
+    searchVal: ""
   },
   onLoad: function (options) {
     var that = this
     app.getShuttlBusList(function (ShuttleBusList) {
+      var busList = ShuttleBusList;
+      for (var i = 0; i < busList.length; i++) {
+        busList[i].found = true;
+      }
       that.setData({
-        ShuttleBusList: ShuttleBusList
+        ShuttleBusList: busList
       })
     })
   },
@@ -22,5 +26,48 @@ Page({
     wx.navigateTo({
       url: '../bus/bus?id=' + e.currentTarget.id
     })
+  },
+  //Search
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    let busList = this.data.ShuttleBusList;
+    for (let i = 0; i < busList.length; i++) {
+      busList[i].found = true;
+    }
+    this.setData({
+      searchVal: "",
+      inputShowed: false,
+      ShuttleBusList: busList
+    });
+  },
+  clearInput: function () {
+    let busList = this.data.ShuttleBusList;
+    for (let i = 0; i < busList.length; i++) {
+      busList[i].found = true;
+    }
+    this.setData({
+      ShuttleBusList: busList,
+      searchVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      searchVal: e.detail.value
+    });
+    var busList = this.data.ShuttleBusList;
+    for (var i = 0; i < busList.length;i++){
+      if (JSON.stringify(busList[i]).indexOf(e.detail.value)>-1){
+        busList[i].found = true;
+      }else{
+        busList[i].found = false;
+      }
+    }
+    this.setData({
+      ShuttleBusList: busList
+    });
   }
 })
